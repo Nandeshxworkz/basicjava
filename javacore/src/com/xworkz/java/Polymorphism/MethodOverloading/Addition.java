ddd431a827bef7e395a4215589056cbef5ddbbee
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-01 09:41:05 +0530

    method overloading addition

---
+++ javacore/src/com/xworkz/java/Polymorphism/MethodOverloading/Addition.java
@@ -0,0 +1,11 @@
+package com.xworkz.java.Polymorphism.MethodOverloading;
+
+public class Addition {
+	void add(int a , int b) {
+		System.out.println("Addition of two no's :"+ (a+b));
+	}
+	void add(float a , float b) {
+		System.out.println("Addition of two no's :"+ (a+b));
+	}
+
+}
---
+++ javacore/src/com/xworkz/java/Polymorphism/MethodOverloading/AdditionMethodover.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.Polymorphism.MethodOverloading;
+
+public class AdditionMethodover {
+	public static void main(String[] args) {
+		
+	
+	Addition a1 = new Addition();
+	a1.add(20.10f, 10.90f);
+	a1.add(10, 20);
+	
+	}
+	
+}
