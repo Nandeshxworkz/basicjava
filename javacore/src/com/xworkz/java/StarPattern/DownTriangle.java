4f76c049a1ec7f21089d099421894d444bfd0dad
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-01 09:43:32 +0530

    star pattern down triangle

---
+++ javacore/src/com/xworkz/java/StarPattern/DownTriangle.java
@@ -0,0 +1,21 @@
+package com.xworkz.java.StarPattern;
+
+public class DownTriangle {
+	
+		public static void main(String []args){
+
+		for(int a=1; a<=5; a++) {
+			for(int b=2; b<=a; b++) {
+			System.out.print(" ");	
+			}
+			for(int c=5; c>=a; c--) {
+				System.out.print(" *");
+			}
+			for(int d=2; d<=a; d++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		}
+		}
+
