20c79c63236cad7b9ffd7f6b65a966a4abc3ced1
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-01 09:42:26 +0530

    aggegation Human and heart

---
+++ javacore/src/com/xworkz/java/Aggregation/Heart.java
@@ -0,0 +1,7 @@
+package com.xworkz.java.Aggregation;
+
+public class Heart {
+	String heartbeat ;
+	int noofchambers ; 
+
+}
---
+++ javacore/src/com/xworkz/java/Aggregation/Human.java
@@ -0,0 +1,8 @@
+package com.xworkz.java.Aggregation;
+
+public class Human {
+	int legs;
+	int hands;
+	Heart heart ;
+
+}
