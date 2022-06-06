d3f9e54f04a2733deba688d81dea79b802f23577
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-06 15:15:47 +0530

    Star pattern

---
+++ javacore/src/com/xworkz/java/StarPattern/NumberswithStar.java
@@ -0,0 +1,25 @@
+package com.xworkz.java.StarPattern;
+
+public class NumberswithStar{
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=5; j>=1;j--) {
+		if(j!=i)
+			System.out.print(j);
+		else
+			System.out.print("*");
+			}
+			System.out.println();
+			
+			}
+		
+		}
+
+	}
+	
+	
+
+
+
