f876c3a7c8e75daea66e2a416fbe1bb68b8aeaea
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-08 17:35:02 +0530

    sum of natural numbers

---
+++ javacore/src/com/xworkz/java/BasicJavaPrograms/SumNaturalNumbers/SumNaturalNo2.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.BasicJavaPrograms.SumNaturalNumbers;
+
+public class SumNaturalNo2 {
+	public static void main(String[] args) {
+	
+	int i=1, sum=0;
+	while(i<=100) {
+		sum=sum+i;
+		i++;
+	}
+	System.out.println("Sum of 100 natural numbers is="+ sum);
+	}
+}
