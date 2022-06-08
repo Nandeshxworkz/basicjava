0d43038ed8247e634bf5a83b2b54ee00df50f988
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-08 17:28:38 +0530

    sum

---
+++ javacore/src/com/xworkz/java/BasicJavaPrograms/SumNaturalNumbers/SumNaturalNo1.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.BasicJavaPrograms.SumNaturalNumbers;
+
+public class SumNaturalNo1 {
+	public static void main(String[] args) {
+		
+	
+	int sum=0;
+	for(int i = 1; i<=10; i++) {
+	     sum +=i;
+		}
+	 System.out.println("Sum of 10 natural numbers is ="+ sum);
+	}
+}
