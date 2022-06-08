237e47d01b0948b15a34194de505a84b2c02c804
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-08 17:50:08 +0530

    Number is positive or negative

---
+++ javacore/src/com/xworkz/java/BasicJavaPrograms/NumberisPositiveorNegative/NumberisPositive1.java
@@ -0,0 +1,15 @@
+package com.xworkz.java.BasicJavaPrograms.NumberisPositiveorNegative;
+
+public class NumberisPositive1 {
+	public static void main(String[] args) {
+	
+	int x=-11;
+	
+	if(x>0)
+		System.out.println("The number is positive");
+	else if(x<0)
+		System.out.println("The number is negative");
+	else
+		System.out.println(" The number is zero");
+	}
+}
