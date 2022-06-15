e64282c9a65bf7105d9bb586eb8188a54f6c8426
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-15 22:14:41 +0530

    Assignment on int to char

---
+++ javacore/src/com/xworkz/java/typecasting/primitivetypecasting/Assignment/Explici1.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.typecasting.primitivetypecasting.Assignment;
+
+public class Explici1 {
+	public static void main(String[] args) {
+		
+		int A = 69;
+		char c = (char) A;
+		
+		System.out.println("a:"+ A);
+		System.out.println("c:"+ c);
+	}
+
+}
---
+++ javacore/src/com/xworkz/java/typecasting/primitivetypecasting/Assignment/Imlicit1.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.typecasting.primitivetypecasting.Assignment;
+
+public class Imlicit1 {
+	public static void main(String[] args) {
+		
+		char c = 'A';
+		int d= c;
+		
+		System.out.println("c:"+ c);
+		System.out.println("d:"+ d);
+	}
+
+}
