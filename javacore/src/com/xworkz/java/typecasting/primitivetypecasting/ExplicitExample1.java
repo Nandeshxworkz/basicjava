0779762b9719f97cfdbaa54d1f9796dbef901c0b
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-15 22:11:34 +0530

    primitive

---
+++ javacore/src/com/xworkz/java/typecasting/primitivetypecasting/ExplicitExample1.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.typecasting.primitivetypecasting;
+
+public class ExplicitExample1 {
+	public static void main(String[] args) {
+		
+		long x= 9999999999l;
+		int y =(int)x;
+		
+		System.out.println("x:"+x);
+		System.out.println("y:"+y);
+	}
+
+}
---
+++ javacore/src/com/xworkz/java/typecasting/primitivetypecasting/ImplicitExample1.java
@@ -0,0 +1,14 @@
+
+package com.xworkz.java.typecasting.primitivetypecasting;
+
+public class ImplicitExample1 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		long b=a;
+		
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+	}
+}
