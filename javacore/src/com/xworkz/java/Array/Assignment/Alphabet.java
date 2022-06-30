b6fa78de9063651915432d154f4d5f0fbc15c79c
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-06-30 23:56:58 +0530

    Array= to printing the alphabets

---
+++ javacore/src/com/xworkz/java/Array/Assignment/Alphabet.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.Array.Assignment;
+
+public class Alphabet {
+	public static void main(String[] args) {
+		char[] alphabet = new char[26];
+		
+		for(int i = 0; i<alphabet.length;i++) {
+			System.out.print((char)(65+i)+" ");
+			
+		}
+	}
+
+}
