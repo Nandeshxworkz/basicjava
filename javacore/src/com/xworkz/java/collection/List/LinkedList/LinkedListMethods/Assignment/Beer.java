64669ad49ab1da3a3cc3bf1693040091384f103a
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-07-21 01:40:07 +0530

    Assignment on linkedlist

---
+++ javacore/src/com/xworkz/java/collection/List/LinkedList/LinkedListMethods/Assignment/Beer.java
@@ -0,0 +1,43 @@
+package com.xworkz.java.collection.List.LinkedList.LinkedListMethods.Assignment;
+
+public class Beer {
+private	String taste;
+private	double price;
+private	String color;
+	
+  Beer(){
+	  
+  }
+
+public String getTaste() {
+	return taste;
+}
+
+public void setTaste(String taste) {
+	this.taste = taste;
+}
+
+public double getPrice() {
+	return price;
+}
+
+public void setPrice(double price) {
+	this.price = price;
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+
+@Override
+public String toString() {
+	return "Beer [taste=" + taste + ", price=" + price + ", color=" + color + "]";
+}
+  
+  
+	
+}
---
+++ javacore/src/com/xworkz/java/collection/List/LinkedList/LinkedListMethods/Assignment/LinkedListCustomDemo.java
@@ -0,0 +1,32 @@
+package com.xworkz.java.collection.List.LinkedList.LinkedListMethods.Assignment;
+
+import java.util.LinkedList;
+
+public class LinkedListCustomDemo {
+	public static void main(String[] args) {
+	
+	
+	
+	Beer beer = new Beer();
+	beer.setTaste(" good");
+	beer.setPrice(180);
+	beer.setColor("brown");
+	
+	Beer kf = new Beer();
+	kf.setColor("yellowish");
+	kf.setPrice(190);
+	kf.setTaste("sour");
+	
+	LinkedList<Beer> ll = new LinkedList<Beer>();
+     ll.add(kf);
+	 ll.add(beer);
+	 System.out.println(ll);
+	 
+	 for(Beer ll1:ll) {
+		 if(ll1.getPrice()==180) {
+			 System.out.println(ll1);
+		 }
+		 
+	 }
+	}
+}
