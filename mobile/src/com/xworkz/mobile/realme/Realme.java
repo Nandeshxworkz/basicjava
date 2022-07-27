2dd75df54b10e7ac4a978ed08ff14fa8ad8ba0ce
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-07-28 00:15:42 +0530

    Assignment 2

---
+++ mobile/src/com/xworkz/mobile/realme/Realme.java
@@ -0,0 +1,13 @@
+package com.xworkz.mobile.realme;
+
+public class Realme {
+	
+public static void main(String[] args) {
+	System.out.println("Purchasing realme brand");
+	
+	 Vendor vendor = new Vendor();
+	 String v1 =vendor.getName();
+	 System.out.println(v1);
+}
+
+}
---
+++ mobile/src/com/xworkz/mobile/realme/Vendor.java
@@ -0,0 +1,13 @@
+package com.xworkz.mobile.realme;
+
+public class Vendor {
+	
+	public Vendor() {
+		System.out.println("Mobile price is 29999");
+	}
+	
+	public String getName() {
+		return "Realme";
+	}
+
+}
