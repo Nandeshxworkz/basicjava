7122886a10e2297e971e85a67b499da98a635bf2
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-08-04 10:35:15 +0530

    Assignment on job edited

--- job/src/com/xworkz/job/dao/JobDAOImpl.java
+++ job/src/com/xworkz/job/dao/JobDAOImpl.java
@@ -5,8 +5,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.xworkz.job.constants.DBProperties.*;
+
 
 import com.xworkz.job.constants.DesignationType;
 import com.xworkz.job.constants.QualificationType;
@@ -60,12 +63,12 @@ public class JobDAOImpl implements JobDAO{
 				String qual = resultset.getString(4);
 				Double perctage = resultset.getDouble(5);
 				String fresh = resultset.getString(6);
-				
+				System.out.println(id1+" "+dstn+" "+ lpa+" "+qual+" "+perctage+" "+fresh);
 				JobDTO jobDTO = new JobDTO();
 				jobDTO.setId(id1);
-				jobDTO.setType(DesignationType.valueOf(dstn));
+				jobDTO.setType(DesignationType.getValueBy(dstn));
 				jobDTO.setPackages(lpa);
-				jobDTO.setName(QualificationType.valueOf(qual));
+				jobDTO.setName(QualificationType.getByValue(qual));
 				jobDTO.setPercentage(perctage);
 				jobDTO.setFresher(fresh);
 				return jobDTO;
@@ -81,31 +84,163 @@ public class JobDAOImpl implements JobDAO{
 
 	@Override
 	public JobDTO findByIdAndDesignation(Integer id, String designation) {
-		// TODO Auto-generated method stub
+           try {
+			Connection connection = DriverManager.getConnection(URL.getValue(),USERNAME.getValue(),SECRET.getValue());
+	        String search1 = "select * from job.job_info where id=? and Designation=? ";
+	      //  System.out.println("search1 :"+ search1)
+			
+			PreparedStatement statement = connection.prepareStatement(search1);
+			statement.setInt(1, id);
+			statement.setString(2, designation);
+			
+			ResultSet resultset = statement.executeQuery(); 
+			//System.out.println("resultSet check :"+ resultset);
+	
+			while(resultset.next()){
+				Integer id1 = resultset.getInt(1);
+				String dstn = resultset.getString(2);
+				Double lpa = resultset.getDouble(3);
+				String qual = resultset.getString(4);
+				Double perctage = resultset.getDouble(5);
+				String fresh = resultset.getString(6); 
+				//System.out.println(" 2nd time get :"+id+" "+dstn+" "+ lpa+" "+qual+" "+perctage+" "+fresh);
+				JobDTO jobDTO = new JobDTO();
+				jobDTO.setId(id1);
+				jobDTO.setType(DesignationType.getValueBy(dstn));
+				jobDTO.setPackages(lpa);
+				jobDTO.setName(QualificationType.getByValue(qual));
+				jobDTO.setPercentage(perctage);
+				jobDTO.setFresher(fresh);
+				return jobDTO;
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@Override
-	public JobDTO findByIdAndDesigantionAndQualification(Integer id, String desigantion, String qualification) {
-		// TODO Auto-generated method stub
+	public JobDTO findByIdAndDesigantionAndQualification(Integer id, String designation, String qualification) {
+		try {
+			Connection connection = DriverManager.getConnection(URL.getValue(),USERNAME.getValue(),SECRET.getValue());
+			 String search1 = "select * from job.job_info where id=? and Designation=? and Qualification=?";
+		      //  System.out.println("search1 :"+ search1)
+				
+				PreparedStatement statement = connection.prepareStatement(search1);
+				statement.setInt(1, id);
+				statement.setString(2, designation);
+				statement.setString(3, qualification);
+				ResultSet resultset = statement.executeQuery(); 
+				//System.out.println("resultSet check :"+ resultset);
+		
+				while(resultset.next()){
+					Integer id1 = resultset.getInt(1);
+					String dstn = resultset.getString(2);
+					Double lpa = resultset.getDouble(3);
+					String qual = resultset.getString(4);
+					Double perctage = resultset.getDouble(5);
+					String fresh = resultset.getString(6); 
+					//System.out.println(" 2nd time get :"+id+" "+dstn+" "+ lpa+" "+qual+" "+perctage+" "+fresh);
+					JobDTO jobDTO = new JobDTO();
+					jobDTO.setId(id1);
+					jobDTO.setType(DesignationType.getValueBy(dstn));
+					jobDTO.setPackages(lpa);
+					jobDTO.setName(QualificationType.getByValue(qual));
+					jobDTO.setPercentage(perctage);
+					jobDTO.setFresher(fresh);
+					return jobDTO;
+				}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public int getTotal() {
-		// TODO Auto-generated method stub
-		return 0;
+       try {
+		Connection connection = DriverManager.getConnection(URL.getValue(),USERNAME.getValue(),SECRET.getValue());
+	    String query = "select * from job.job_info ";
+	    PreparedStatement statement = connection.prepareStatement(query);
+	    ResultSet rs =  statement.executeQuery();
+	    AtomicInteger auto = new AtomicInteger();
+	    while(rs.next()) {
+	    	auto.incrementAndGet();
+	    	}
+	    System.out.println("No of rows in Database :"+auto);
+       } catch (SQLException e) {
+		e.printStackTrace();
+	}
+    		   
+    		   
+		return 0; 
 	}
 
 	@Override
 	public String isFresherById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection connection = DriverManager.getConnection(URL.getValue(),USERNAME.getValue(),SECRET.getValue());
+		    String query = "select fresher from job.job_info where "+" id ="+id;
+		   Statement statement =connection.createStatement();
+		 //   PreparedStatement statement = connection.prepareStatement(query);
+		   // statement.setInt(1, id);
+		    ResultSet resultset = statement.executeQuery(query);
+		    while(resultset.next()) {
+		    String fresh =resultset.getString(1);
+		    return fresh;
+		    }
+		    
+//		    while(resultset.next()) {
+//		    	Integer id1 = resultset.getInt(1);
+//				String dstn = resultset.getString(2);
+//				Double lpa = resultset.getDouble(3);
+//				String qual = resultset.getString(4);
+//				Double perctage = resultset.getDouble(5);
+//				String fresh = resultset.getString(6); 
+//				System.out.println(" 2nd time get :"+id+" "+dstn+" "+ lpa+" "+qual+" "+perctage+" "+fresh);
+//				JobDTO jobDTO = new JobDTO();
+//				jobDTO.setId(id1);
+//				jobDTO.setType(DesignationType.getValueBy(dstn));
+//				jobDTO.setPackages(lpa);
+//				jobDTO.setName(QualificationType.getByValue(qual));
+//				jobDTO.setPercentage(perctage);
+//				jobDTO.setFresher(fresh);
+//				System.out.println(" hi");
+//				return "";
+//				
+//		    }
+		    
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return "No data found";
 	}
 
 	@Override
 	public Double getmaxPercentage() {
-		// TODO Auto-generated method stub
+      try {
+		Connection connection = DriverManager.getConnection(URL.getValue(),USERNAME.getValue(),SECRET.getValue());
+        String query = "select Percentage from job.job_info";
+       // PreparedStatement statement = connection.prepareStatement(query);
+        Statement statement = connection.createStatement();
+        ResultSet resultset = statement.executeQuery(query);
+        double max=0;
+        while(resultset.next()) {
+        	double percentage = resultset.getDouble(1);
+        	if(percentage>max)
+        		max=percentage;
+        } return max;
+        
+        // Boolean b =resultset.next();
+      //   System.out.println(b);
+      }catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 		return null;
 	}
 
--- job/src/com/xworkz/job/runner/JobRunner.java
+++ job/src/com/xworkz/job/runner/JobRunner.java
@@ -8,12 +8,29 @@ import com.xworkz.job.dto.JobDTO;
 
 public class JobRunner {
 	public static void main(String[] args) {
-		JobDTO jobDTO = new JobDTO(2,DesignationType.TRAINEE,250000.00D,QualificationType.BCA,85.00D,"NO");
+		JobDTO jobDTO = new JobDTO(4,DesignationType.FULL_STACK_ENGINEER,500000.00D,QualificationType.BTECH,45.00D,"Yes");
 		
 		JobDAO jobDAO = new JobDAOImpl();
-		//jobDAO.save(jobDTO);
+	   // jobDAO.save(jobDTO);
 		
-		jobDAO.findById(1);
+	JobDTO	jobdto =jobDAO.findById(2);
+	System.out.println(jobdto);
+	
+	JobDTO jobdto1=jobDAO.findByIdAndDesignation(1, "se");
+	System.out.println(jobdto1);
+	
+	JobDTO jobdto2= jobDAO.findByIdAndDesigantionAndQualification(3, "ase", "MTech");
+	System.out.println(jobdto2);
+	
+	         jobDAO.getTotal();
+	         
+	         System.out.println("---------------------");
+	  String dto4  = jobDAO.isFresherById(4);    
+	        System.out.println(" fresher :"+dto4);
+	        
+	        System.out.println("--------");
+	       Double bs=jobDAO.getmaxPercentage();
+	       System.out.println(bs);  
 		
 	}
 
