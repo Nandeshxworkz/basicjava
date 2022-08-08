d9ab84125dc148e2f61a424e38c361b83ce86a0c
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-08-08 22:54:32 +0530

    resort

--- resort/src/com/xworkz/resort/entity/ResortEntity.java
+++ resort/src/com/xworkz/resort/entity/ResortEntity.java
@@ -3,11 +3,18 @@ package com.xworkz.resort.entity;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
-
+@Entity
+@Table(name="resort_info")
 public class ResortEntity {
+	
+	@Column(name="id")
 	
 	private int id;
 	private String name;
