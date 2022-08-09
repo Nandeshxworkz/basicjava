230d70b044bf590340222525cb01b94112f19ae9
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-08-09 22:56:20 +0530

    resort

--- resort/src/com/xworkz/resort/entity/ResortEntity.java
+++ resort/src/com/xworkz/resort/entity/ResortEntity.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -14,17 +15,26 @@ import lombok.Data;
 @Table(name="resort_info")
 public class ResortEntity {
 	
+	@Id
 	@Column(name="id")
-	
 	private int id;
+	@Column(name="name")
 	private String name;
+	@Column(name="location")
 	private String location;
+	@Column(name="checkInTime")
 	private LocalTime checkInTime;
+	@Column(name="checkOutTime")
 	private LocalTime checkOutTime;
+	@Column(name="createBy")
 	private String createBy;
+	@Column(name="createDate")
 	private LocalDate createDate;
+	@Column(name="updateDate")
 	private LocalDate updateDate;
+	@Column(name="owner")
 	private String owner;
+	@Column(name="pricePerDay")
 	private double pricePerDay;
 	
 
